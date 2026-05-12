f59f69aebfeb47e135375fe1a8f36057e53ee5db
Slavomir <Slavomir7115@gmail.com>
2026-05-12 13:59:29 +0200

    added padding

--- app/ochrana-suborov-cookies/page.jsx
+++ app/ochrana-suborov-cookies/page.jsx
@@ -9,7 +9,7 @@ const CookiePolicy = () => {
   }, []);
 
   return (
-    <Container>
+    <Container className="py-20">
       <div className="max-w-4xl mx-auto px-6 py-12 font-body leading-relaxed text-foreground">
         <h1 className="text-3xl md:text-4xl font-bold mb-6 uppercase tracking-widest text-foreground">
           Zásady používania súborov cookie
