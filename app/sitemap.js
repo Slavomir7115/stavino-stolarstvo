325cc77c31c870937c4d74f297f83e9cc5e5a15c
Slavomir <Slavomir7115@gmail.com>
2026-05-12 14:09:02 +0200

    added pages to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const baseUrl = "https://www.stavino.sk";
+  const baseUrl = "https://www.stavino-stolarstvo.sk";
 
   return [
     {
@@ -7,6 +7,18 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/ochrana-osobnych-udajov`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/ochrana-suborov-cookies`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
