d3b093ecfbcac2aaae6e7a426885125c7dd7b47b
Slavomir <Slavomir7115@gmail.com>
2026-05-12 14:09:51 +0200

    fixed bug in robots

--- app/robots.js
+++ app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
       allow: "/",
       disallow: "/admin",
     },
-    sitemap: "https://www.stavino.sk/sitemap.xml",
+    sitemap: "https://www.stavino-stolarstvo.sk/sitemap.xml",
   };
 }
